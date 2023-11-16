e3201e47a97ebcada03fa953d79d61cd6ff39876
Asus <vaxa2320@gmail.com>
2023-11-16 20:49:33 +0300

    user

---
+++ src/main/java/by/IT/ArrayParameterServlet.java
@@ -0,0 +1,24 @@
+package by.IT;
+
+import javax.security.sasl.SaslException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Hashtable;
+
+@WebServlet(name = "arrayParameterServlet", urlPatterns = "/arrParams")
+public class ArrayParameterServlet extends HttpServlet {
+    @Override
+    protected  void doGet(HttpServletRequest req, HttpServletResponse resp) throws SaslException, IOException{
+        String[] parames = req.getParameterValues( "parame");
+        PrintWriter writer= resp.getWriter();
+        Arrays.stream(parames).forEach(it -> writer.write("<h1>"+ it + "</h1"));
+    }
+
+
+
+}
---
+++ src/main/java/by/IT/MapParameterServlet.java
@@ -0,0 +1,24 @@
+package by.IT;
+
+import javax.security.sasl.SaslException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Map;
+
+@WebServlet(name = "MapParameterServlet", urlPatterns = "/mapParam")
+public class MapParameterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws SaslException, IOException {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        PrintWriter writer = resp.getWriter();
+        parameterMap.forEach((k, v) -> writer.write("<h1>" + k + "" + Arrays.toString(v) + "</h1>"));
+    }
+}
+
+
+///−−•• •− •−−• −•−− − −−−•• −−−−− −−−•• −−−−−
