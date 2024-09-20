57b7aff9febd0c79cb151425087a262db556f035
Asus <vaxa2320@gmail.com>
2024-09-20 15:39:30 +0300

    user5

--- src/main/java/by/IT/main4.java
+++ src/main/java/by/IT/main4.java
@@ -1,6 +1,6 @@
 package by.IT;
 public class main4 {
     public static void main (String[] arg){
-        System.out.println("______________1234________________");
+        System.out.println("______________12345________________");
     }
 }
